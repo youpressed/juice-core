a4fa12f86e195202fde391b4fb974488b0af4321
Aram Zadikian <brancusi@gmail.com>
2017-05-23 22:54:16 -0700

    wip

--- app/services/pdf-generator.js
+++ app/services/pdf-generator.js
@@ -3,12 +3,22 @@ import config from 'juice-core/config/environment';
 
 export default Ember.Service.extend({
   generateFullPrepSheet(production) {
+    const sample = {
+      "pdfData": [
+        {
+          "label": "kale",
+          "weight": 1
+        }
+      ]
+    };
+
     return Ember.$.ajax({
       url: config.docService.allDocsEndpoint,
       type:"POST",
       headers: {
         'Content-Type': 'application/json'
-      }
+      },
+      data: JSON.stringify(sample)
      });
 
   }

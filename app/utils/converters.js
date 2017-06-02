4cffe15e80a26ab5dbe333998b283876cdf36d75
Aram Zadikian <brancusi@gmail.com>
2017-06-02 12:46:03 -0700

    wip

--- app/utils/converters.js
+++ app/utils/converters.js
@@ -1,16 +1,20 @@
 import { units } from 'juice-core/constants/unit-conversions';
 import _ from 'lodash';
 
-const toBest = (q, from, allowed) => {
+const buildAllowedUoms = (from, allowed) => {
   const uoms = units[from].list;
   const allowedUoms = allowed || uoms.map(uom => uom.uom);
-  const allowedUnits = units[from].list.filter(uom => allowedUoms.includes(uom.uom));
+  return units[from].list.filter(uom => allowedUoms.includes(uom.uom));
+}
+
+const toBest = (q, from, allowed) => {
+  const uoms = buildAllowedUoms(from, allowed);
 
   const castQ = parseFloat(q);
   const baseData = uom(castQ, from).toBase();
 
   const sorted = _
-    .map(allowedUnits, uom => {
+    .map(uoms, uom => {
       return {
         uom: uom.uom,
         q: (baseData.q / uom.factor)
@@ -19,6 +23,8 @@ const toBest = (q, from, allowed) => {
     .sort((a, b) => Math.abs(1-a.q) - Math.abs(1-b.q))
     .sort((a, b) => {
       if((a.q >= 1) && (b.q >= 1)) {
+        return 0;
+      } else if((a.q < 1) && (b.q < 1)) {
         return 0;
       } else if((a.q >= 1) && (b.q < 1)) {
         return -1;
@@ -31,12 +37,11 @@ const toBest = (q, from, allowed) => {
 }
 
 const toMixed = (q, from, allowed) => {
-  const uoms = units[from].list;
-  const allowedUoms = allowed || uoms.map(uom => uom.uom);
-  const allowedUnits = units[from].list.filter(uom => allowedUoms.includes(uom.uom));
-  const firstPass = toBest(q, from, allowedUoms);
+  const uoms = buildAllowedUoms(from, allowed);
+  const firstPass = toBest(q, from, allowed);
 
-  const firstUnit = {q:Math.floor(firstPass.q), uom:firstPass.uom};
+  const firstUnit = {q: Math.floor(firstPass.q), uom:firstPass.uom};
+
   const remainder = firstPass.q - firstUnit.q;
 
   let secondUnit;
@@ -44,21 +49,27 @@ const toMixed = (q, from, allowed) => {
   if(remainder > 0) {
     const uoms = units[firstUnit.uom].list;
     const currentUom = uoms.find(uom => uom.uom === firstUnit.uom)
-    const nextIndex = allowedUnits.indexOf(currentUom) - 1;
-    const nextUom = allowedUnits[nextIndex];
+    const nextIndex = uoms.indexOf(currentUom) - 1;
+    const nextUom = uoms[nextIndex];
 
     if(nextUom !== undefined) {
       const next = uom(remainder, firstUnit.uom).to(nextUom.uom);
-      secondUnit = {q: Math.floor(next), uom:nextUom.uom};
+      secondUnit = {q: next, uom:nextUom.uom};
     }
   }
 
-  return [
+  let results = [
     firstUnit,
     secondUnit
   ]
   .filter(measure => measure !== undefined)
   .filter(measure => measure.q > 0);
+
+  if(results.length < 1) {
+    results = [{q: Math.ceil(firstPass.q), uom:firstPass.uom}];
+  }
+
+  return results;
 }
 
 const uom = (q, from) => {

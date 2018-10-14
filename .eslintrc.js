2cd2d96068670edac6e52be058ba4b83d17da002
Aram Zadikian <brancusi@gmail.com>
2018-10-14 12:14:13 -0700

    updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,26 @@ module.exports = {
     "math": true,
     "moment": true,
     "Set": true
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    }
+  ]
 };

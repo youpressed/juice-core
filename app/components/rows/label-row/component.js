02ef4e7877d15bfbe75a82ddae651d8d0dbc71bb
Aram Zadikian <brancusi@gmail.com>
2018-10-18 11:34:09 -0700

    added active filtering, cleanup

--- app/components/rows/label-row/component.js
+++ app/components/rows/label-row/component.js
@@ -1,22 +1,7 @@
 import Ember from 'ember';
 
-const {
-  computed: {
-    notEmpty
-  }
-} = Ember;
-
 export default Ember.Component.extend({
-
-  hasDestroyAction: notEmpty('destroyAction'),
-
   click() {
     this.get('clickAction')(this.get('model'));
-  },
-
-  actions: {
-    handleDestroyClicked() {
-      this.get('destroyAction')(this.get('model'));
-    }
   }
 });

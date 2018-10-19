78dd8c3b87c8cf8910996245d9f250cc3636b975
Aram Zadikian <brancusi@gmail.com>
2018-10-18 22:18:55 -0700

    fixed count error

--- app/components/rows/date-label-row/component.js
+++ app/components/rows/date-label-row/component.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { computed } from '@ember/object';
 
 const {
   computed: {
@@ -9,6 +10,12 @@ const {
 export default Ember.Component.extend({
   hasDestroyAction: notEmpty('destroyAction'),
   isShowingExtrasMenu: false,
+
+  totalCount: computed('model.children.@each.{q,sign}', function() {
+    return this.get('model.children')
+      .filter(edge => edge.get('sign') !== -1)
+      .reduce((acc, cur) => acc + (parseFloat(cur.get('q')) || 0), 0);
+  }),
 
   click() {
     this.get('clickAction')(this.get('model'));

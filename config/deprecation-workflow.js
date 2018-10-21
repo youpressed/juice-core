39d307c675b7f415ea3c366b0a7414892fce7865
Aram Zadikian <brancusi@gmail.com>
2018-10-21 13:46:36 -0700

    removed

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,12 +1,7 @@
 window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
-    // { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable" },
-    // { handler: "silence", matchId: "ember-component.send-action" }
-
-    // { handler: "silence", matchId: "ds.serializer.private-should-serialize-has-many" },
-    // { handler: "silence", matchId: "ember-metal.model_factory_injections" },
-    // { handler: "silence", matchId: "ember-views.lifecycle-hook-arguments" },
-    // { handler: "silence", matchId: "ember-router.router" }
+    { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable" },
+    { handler: "silence", matchId: "ember-component.send-action" }
   ]
 };

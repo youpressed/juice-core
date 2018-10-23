cb49794a47fcbe6b859c7a68703ab0570a7e4a96
Aram Zadikian <brancusi@gmail.com>
2018-10-22 20:45:09 -0700

    test at

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -1,11 +1,13 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
+import { visit,
+  // currentURL
+ } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 import fireBaseFixture from '../fixtures/firebase-default';
 import sessionFixture from '../fixtures/session-default';
 
-import { authenticateSession } from 'ember-simple-auth/test-support';
+// import { authenticateSession } from 'ember-simple-auth/test-support';
 
 import {
   mockFirebase,
@@ -19,23 +21,105 @@ import {
 module('Acceptance | login', function(hooks) {
   setupApplicationTest(hooks);
 
+  hooks.afterEach(() => {
+    // console.log('');
+  });
+
   test('logs user and sets correct tenant data', async function(assert) {
+    await mockAuth0Lock(this, sessionFixture);
+
     mockAuthFirebase(this);
     mockFirebase(this, fireBaseFixture);
-
-    await mockAuth0Lock(this, sessionFixture);
 
     await visit('/login');
 
     assert.dom('[data-test-id="date-label-row"]').exists({count: 1})
   });
 
-  test('defaults to landing page on login', async function(assert) {
-    mockFirebase(this);
-    authenticateSession();
+  test('logs user and sets correct tenant data', async function(assert) {
+    await mockAuth0Lock(this, sessionFixture);
+
+    mockAuthFirebase(this);
+    mockFirebase(this, fireBaseFixture);
 
     await visit('/login');
 
-    assert.equal(currentURL(), '/productions');
+    assert.dom('[data-test-id="date-label-row"]').exists({count: 1})
   });
+
+  test('logs user and sets correct tenant data', async function(assert) {
+    await mockAuth0Lock(this, sessionFixture);
+
+    mockAuthFirebase(this);
+    mockFirebase(this, fireBaseFixture);
+
+    await visit('/login');
+
+    assert.dom('[data-test-id="date-label-row"]').exists({count: 1})
+  });
+
+  test('logs user and sets correct tenant data', async function(assert) {
+    await mockAuth0Lock(this, sessionFixture);
+
+    mockAuthFirebase(this);
+    mockFirebase(this, fireBaseFixture);
+
+    await visit('/login');
+
+    assert.dom('[data-test-id="date-label-row"]').exists({count: 1})
+  });
+
+  test('logs user and sets correct tenant data', async function(assert) {
+    await mockAuth0Lock(this, sessionFixture);
+
+    mockAuthFirebase(this);
+    mockFirebase(this, fireBaseFixture);
+
+    await visit('/login');
+
+    assert.dom('[data-test-id="date-label-row"]').exists({count: 1})
+  });
+
+  test('logs user and sets correct tenant data', async function(assert) {
+    await mockAuth0Lock(this, sessionFixture);
+
+    mockAuthFirebase(this);
+    mockFirebase(this, fireBaseFixture);
+
+    await visit('/login');
+
+    assert.dom('[data-test-id="date-label-row"]').exists({count: 1})
+  });
+
+  test('logs user and sets correct tenant data', async function(assert) {
+    await mockAuth0Lock(this, sessionFixture);
+
+    mockAuthFirebase(this);
+    mockFirebase(this, fireBaseFixture);
+
+    await visit('/login');
+
+    assert.dom('[data-test-id="date-label-row"]').exists({count: 1})
+  });
+
+  test('logs user and sets correct tenant data', async function(assert) {
+    await mockAuth0Lock(this, sessionFixture);
+
+    mockAuthFirebase(this);
+    mockFirebase(this, fireBaseFixture);
+
+    await visit('/login');
+
+    assert.dom('[data-test-id="date-label-row"]').exists({count: 1})
+  });
+  
+
+  // test('defaults to landing page on login', async function(assert) {
+  //   mockFirebase(this);
+  //   authenticateSession();
+  //
+  //   await visit('/login');
+  //
+  //   assert.equal(currentURL(), '/productions');
+  // });
 });

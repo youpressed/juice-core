38e68c6f75467aa4696af0fbe73f23084d46c7e9
Vinh Luu <vinhlt3@gmail.com>
2018-10-29 10:48:30 +0700

    add spec for edit production

--- tests/acceptance/productions-test.js
+++ tests/acceptance/productions-test.js
@@ -1,20 +1,29 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
-import fireBaseFixture from '../fixtures/firebase-production-default';
+import { visit, click } from '@ember/test-helpers';
+import fireBaseFixture from 'juice-core/tests/fixtures/firebase-default';
 
 import {
   initAcceptanceTest
-} from '../helpers/acceptance-helpers';
+} from 'juice-core/tests/helpers/acceptance-helpers';
 
 module('Acceptance | productions', function(hooks) {
   initAcceptanceTest(hooks, fireBaseFixture);
 
-  hooks.beforeEach(async () => {
+  test('shows date and total units of productions', async function(assert) {
     await visit('/productions');
-  });
 
-  test('shows date and total units of production', async function(assert) {
     assert.dom('[data-test-date-row-label]').hasText('Sat - 04/07');
     assert.dom('[data-test-date-row-total-count]').hasText('25');
+  });
+
+  test('shows production information correctly', async function (assert) {
+    await visit('/productions/edit/production-id');
+    await click('[data-test-date-row-label]');
+
+    assert.dom('[data-test-production-date-row] input').hasValue('Sat 04/07/18');
+    assert.dom('[data-test-production-name]').exists({ count: 2 });
+
+    assert.dom('[data-test-production-name]').hasText('Tasty Salad');
+    assert.dom('[data-test-production-child-quantity]').hasValue('10');
   });
 });

e531a3f46ab7f0bb92ee498dc24a4e7ac7305133
Vinh Luu <vinhlt3@gmail.com>
2018-11-02 13:47:24 +0700

    use ember-test-selector addon

--- tests/acceptance/productions-test.js
+++ tests/acceptance/productions-test.js
@@ -1,6 +1,5 @@
 import { module, test } from 'qunit';
 import { visit } from '@ember/test-helpers';
-
 import fireBaseFixture from '../fixtures/firebase-production-default';
 
 import {
@@ -15,7 +14,7 @@ module('Acceptance | productions', function(hooks) {
   });
 
   test('shows date and total units of production', async function(assert) {
-    assert.dom('[data-test-id="date-row-date-label"]').hasText('Sun - 04/08');
-    assert.dom('[data-test-id="date-row-total-count"]').hasText('25');
+    assert.dom('[data-test-date-row-label]').hasText('Sun - 04/08');
+    assert.dom('[data-test-date-row-total-count]').hasText('25');
   });
 });

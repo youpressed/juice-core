7b4b01f801ff457f3c99977fe9017b665a74c391
Vinh Luu <vinhlt3@gmail.com>
2018-11-02 13:16:27 +0700

    Implement search

--- config/dotenv.js
+++ config/dotenv.js
@@ -9,7 +9,11 @@ module.exports = function() {
       'DATABASE_URL',
       'INTERCOM_APP_ID',
       'GOOGLE_TRACKING_ID',
-      'STORAGE_BUCKET'
+      'STORAGE_BUCKET',
+      'ALGOLIA_APPLICATION_ID',
+      'ALGOLIA_SEARCH_API_ID',
+      'GRAND_CENTRAL_FB_DATABASE_URL',
+      'GRAND_CENTRAL_FB_ORG_ID'
     ],
     // Fail build when there is missing any of clientAllowedKeys environment variables.
     // By default false.

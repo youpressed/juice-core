592c1581328c9b133d2e29bf92238e870d0a3b7f
Aram Zadikian <brancusi@gmail.com>
2018-11-01 19:04:59 -0700

    added ember-paper

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,6 +2,7 @@ window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
     { handler: "silence", matchId: "ember-runtime.deprecate-copy-copyable" },
-    { handler: "silence", matchId: "ember-component.send-action" }
+    { handler: "silence", matchId: "ember-component.send-action" },
+    { handler: "silence", matchId: "ember-views.event-dispatcher.jquery-event" }
   ]
 };

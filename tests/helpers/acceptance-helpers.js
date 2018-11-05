a86036e5c93734fcf5c37e6ce42aa7673bdee764
Aram Zadikian <brancusi@gmail.com>
2018-11-04 17:46:47 -0800

    adding mirage

--- tests/helpers/acceptance-helpers.js
+++ tests/helpers/acceptance-helpers.js
@@ -12,9 +12,12 @@ import {
   mockAuth
 } from './auth-helpers';
 
+import setupMirageTest from 'ember-cli-mirage/test-support/setup-mirage';
+
 export function initAcceptanceTest(hooks, fireBaseFixture) {
   setupApplicationTest(hooks);
   clearLocalStorage(hooks);
   mockAuth(hooks);
   mockFirebaseData(hooks, fireBaseFixture);
+  setupMirageTest(hooks);
 }

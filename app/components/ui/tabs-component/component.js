7ec080f721933440e1726494e4cb07756579e197
Vinh Luu <vinhlt3@gmail.com>
2018-11-08 14:28:27 +0700

    Implement logic of tabs control

---
+++ app/components/ui/tabs-component/component.js
@@ -0,0 +1,18 @@
+import Component from '@ember/component';
+import _ from 'lodash';
+
+export default Component.extend({
+  didInsertElement() {
+    this._super(...arguments);
+
+    let panels = _.map(this.childViews, v => _.pick(v, ['title', 'elementId']));
+    this.set('panels', panels);
+    this.set('activeId', _.first(panels).elementId);
+  },
+
+  actions: {
+    select(elementId) {
+      this.set('activeId', elementId);
+    }
+  }
+});

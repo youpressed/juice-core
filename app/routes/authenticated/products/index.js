e10cd712bd68e35ac302e1e3c54e4d36f4ec1a8d
Aram Zadikian <brancusi@gmail.com>
2018-11-08 22:57:40 -0800

    fixed add product

--- app/routes/authenticated/products/index.js
+++ app/routes/authenticated/products/index.js
@@ -7,39 +7,5 @@ export default Route.extend(AuthenticatedRouteMixin, {
       orderBy: "type",
       equalTo: "product"
     })
-  },
-
-  actions: {
-    showNode(node) {
-      this.transitionTo('authenticated.products.show', node.get('id'));
-    },
-
-    async createNode() {
-      const products = this.controllerFor('products.index').get('model');
-
-      const sortedProducts = products.sortBy('position');
-
-      const firstProduct = sortedProducts[0];
-
-      let nextPosition = -1;
-
-      if(firstProduct !== undefined) {
-        if(firstProduct.get('position') !== undefined) {
-          nextPosition = firstProduct.get('position') - 1;
-        }
-      }
-
-      const node = this.store.createRecord("node", {
-        type:"product",
-        label:"untitled",
-        position: nextPosition,
-        yield: 1,
-        uom: "count"
-      });
-
-      await node.save();
-
-      this.transitionTo('authenticated.products.show', node.get('id'));
-    }
   }
 });

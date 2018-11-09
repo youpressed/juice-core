2fe939420456b745d74683365dd89998019bf2c4
Aram Zadikian <brancusi@gmail.com>
2018-11-09 08:12:14 -0800

    refactored session out of component

--- app/components/navs/account-nav/component.js
+++ app/components/navs/account-nav/component.js
@@ -1,8 +1,5 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
 
 export default Component.extend({
-  classNames: ["absolute pin-t pin-r mr-8 mt-8"],
-
-  session: service()
+  classNames: ["absolute pin-t pin-r mr-8 mt-8"]
 });

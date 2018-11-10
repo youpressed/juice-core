1c86d72cfd506aa0aaf9054521a18bee77d6dfbc
Vinh Luu <vinhlt3@gmail.com>
2018-11-10 18:09:50 +0700

    Implement creating product from home page

--- app/controllers/authenticated/products/index.js
+++ app/controllers/authenticated/products/index.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-  store: service(),
+  nodeService: service(),
 
   actions: {
     showNode(node) {
@@ -10,30 +10,8 @@ export default Controller.extend({
     },
 
     async createNode() {
-      const products = this.get("model");
-      const sortedProducts = products.sortBy('position');
-
-      const firstProduct = sortedProducts[0];
-
-      let nextPosition = -1;
-
-      if(firstProduct !== undefined) {
-        if(firstProduct.get('position') !== undefined) {
-          nextPosition = firstProduct.get('position') - 1;
-        }
-      }
-
-      const node = this.get("store").createRecord("node", {
-        type:"product",
-        label:"untitled",
-        position: nextPosition,
-        yield: 1,
-        uom: "count"
-      });
-
-      await node.save();
-
-      this.transitionToRoute('authenticated.products.show', node.get('id'));
+      let product = await this.get('nodeService').createProduct();
+      this.transitionToRoute('authenticated.products.show', product.get('id'));
     }
   }
 });

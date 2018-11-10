41a070f77a9cba976a729d6cc3b05938a19a1f97
Aram Zadikian <brancusi@gmail.com>
2018-11-10 10:37:07 -0800

    wip

--- app/controllers/authenticated/productions/index.js
+++ app/controllers/authenticated/productions/index.js
@@ -1,7 +1,7 @@
 import { all } from 'rsvp';
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
-import { sort, filterBy } from '@ember/object/computed';
+import { notEmpty, bool, sort, filterBy } from '@ember/object/computed';
 import _ from 'lodash';
 import { inject as service } from '@ember/service';
 import moment from 'moment';
@@ -9,11 +9,18 @@ import moment from 'moment';
 export default Controller.extend({
 
   store: service(),
+  nodeService: service(),
 
   sortByTimestamp: Object.freeze(['ts:desc']),
   sortedProductions: sort('productions', 'sortByTimestamp'),
 
   productions: filterBy('nodes', 'isProduction', true),
+  products: filterBy('nodes', 'isProduct', true),
+
+  hasProductions: notEmpty("productions"),
+  hasProducts: notEmpty("products"),
+
+  canCreateProduction: bool("hasProducts"),
 
   recentProductions: computed('sortedProductions', function() {
     return _.take(this.get('sortedProductions'), 10);
@@ -66,6 +73,11 @@ export default Controller.extend({
         });
 
       node.destroyRecord();
+    },
+
+    async createProduct(){
+      let product = await this.get('nodeService').createProduct();
+      this.transitionToRoute('authenticated.products.show', product.get('id'));
     }
   }
 });

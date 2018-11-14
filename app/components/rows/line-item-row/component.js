89951ef63d1a8162c48922b117e8cea95915a28a
Vinh Luu <vinhlt3@gmail.com>
2018-11-14 22:11:17 +0700

    Impleted multiple delete

--- app/components/rows/line-item-row/component.js
+++ app/components/rows/line-item-row/component.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
-import { notEmpty } from '@ember/object/computed';
 
 export default Component.extend({
-  isDeleteable: notEmpty("handleDestroy")
+  isDestroyable: false
 });

e730c663a8f25ee475f0c9a47b52b3db000cbb85
Aram Zadikian <brancusi@gmail.com>
2018-11-13 18:31:28 -0800

    demo

--- app/components/editors/product-editor/component.js
+++ app/components/editors/product-editor/component.js
@@ -1,7 +1,9 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  autoSaveIndicator: service(),
   validNodes: computed('nodes.@each.{type}', function() {
     return this.get('nodes')
       .filter(n => !n.get('isProduct') && !n.get('isProduction'))
---
+++ app/services/auto-save-indicator.js
@@ -0,0 +1,26 @@
+import Service from '@ember/service';
+import { computed } from '@ember/object';
+import { or } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
+
+export default Service.extend({
+  store: service(),
+
+  init() {
+    const nodes = this.get("store").peekAll("node");
+    const edges = this.get("store").peekAll("edge");
+
+    this.set('nodes', nodes);
+    this.set('edges', edges);
+  },
+
+  hasDirtyNodes: computed("nodes.@each.{hasDirtyAttributes}", function() {
+    return this.get("nodes").any(node => node.get("hasDirtyAttributes"));
+  }),
+
+  hasDirtyEdges: computed("edges.@each.{hasDirtyAttributes}", function() {
+    return this.get("edges").any(edge => edge.get("hasDirtyAttributes"));
+  }),
+
+  isSaving: or("hasDirtyNodes", "hasDirtyEdges")
+});

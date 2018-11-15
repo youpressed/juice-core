11d71cf1a2e37c744182b55939794f072dd6dfd7
Vinh Luu <vinhlt3@gmail.com>
2018-11-15 14:07:56 +0700

    Add case 'be able to open production show page'

--- tests/acceptance/productions-test.js
+++ tests/acceptance/productions-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, click } from '@ember/test-helpers';
+import { visit, click, currentURL } from '@ember/test-helpers';
 import fireBaseFixture from 'juice-core/tests/fixtures/firebase-default';
 
 import {
@@ -9,17 +9,24 @@ import {
 module('Acceptance | Productions', function(hooks) {
   initAcceptanceTest(hooks, fireBaseFixture);
 
-  test('shows date and total units of productions', async function(assert) {
+  hooks.beforeEach(async () => {
     await visit('/a/productions');
+  });
 
+  test('shows date and total units of productions', async function(assert) {
     assert.dom('[data-test-date-row-label]').includesText('04/07/2018');
     assert.dom('[data-test-date-row-total-count]').includesText('10');
   });
 
   test('be able to create a production', async function(assert) {
-    await visit('/a/productions');
     await click('[data-test-create-button]');
 
     assert.dom('[data-test-page-title]').hasText('Edit Productions');
+  });
+
+  test('be able to open production show page', async function(assert) {
+    await click('[data-test-date-row-label]');
+
+    assert.equal(currentURL(), '/a/productions/production-id');
   });
 });

7db60e2e4cb41a6884677229215331adb9a30bed
Vinh Luu <vinhlt3@gmail.com>
2018-11-15 08:20:48 +0700

    Remove useless components

--- app/components/sections/ingredients-index/component.js
+++
@@ -1,6 +0,0 @@
-import Component from '@ember/component';
-import { filterBy } from '@ember/object/computed';
-
-export default Component.extend({
-  ingredients: filterBy('model', 'type', 'ingredient')
-});

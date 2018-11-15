b8dce1557a2e8100c92ba8532dccfdf318635b31
Vinh Luu <vinhlt3@gmail.com>
2018-11-15 09:12:59 +0700

    Added authenticated index test

---
+++ tests/acceptance/authenticated-index-test.js
@@ -0,0 +1,23 @@
+import { module, test } from 'qunit';
+import { visit, click } from '@ember/test-helpers';
+
+import {
+  initAcceptanceTest
+} from 'juice-core/tests/helpers/acceptance-helpers';
+
+module('Acceptance | Authenticated | Index', function(hooks) {
+  initAcceptanceTest(hooks, {});
+
+  hooks.beforeEach(async () => {
+    await visit('/a');
+  });
+
+  test('displays welcome message', async function (assert) {
+    assert.dom('[data-test-header]').hasText('Welcome to You Pressed');
+  });
+
+  test('clicks button for creating a product', async function(assert) {
+    await click('[data-test-create-button]');
+    assert.dom('[data-test-page-title]').hasText('Edit Product');
+  });
+});

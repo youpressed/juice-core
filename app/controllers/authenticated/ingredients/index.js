aefcbe7eba920ee93db4a09676c8ce26ba71270f
Aram Zadikian <az@whatisfood.com>
2021-01-05 19:37:51 -0800

    Adding exclude quick link

--- app/controllers/authenticated/ingredients/index.js
+++ app/controllers/authenticated/ingredients/index.js
@@ -9,6 +9,11 @@ export default Controller.extend({
       this.transitionToRoute('authenticated.ingredients.show', node.get('id'));
     },
 
+    handleUpdate(model, key, val) {
+      model.set(key, val);
+      model.save();
+    },
+
     async createNode() {
       const node = this.get('store').createRecord("node", {
         type:"ingredient",
